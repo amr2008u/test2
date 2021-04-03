892c315e34c8e502d20dc8ca251d05e0c988895e
amr2008u <34633212+amr2008u@users.noreply.github.com>
2021-04-03 19:15:22 +0200

    t

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update
+apt-get install git -y
+apt-get install tmux -y
+apt-get install php -y
+#close apache server
+/etc/init.d/apache2 stop
+git clone https://github.com/amr2008u/test2.git
+cd test2
+#start tmux server in back ground and start php server
+tmux new-session -d -s my_session 'php -S 91.134.238.147:8080  dns_script.php'
