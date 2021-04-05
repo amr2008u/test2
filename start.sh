3bd859815cf698a785ed02f08aa357fdf22175ae
amr2008u <34633212+amr2008u@users.noreply.github.com>
2021-04-05 03:07:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,3 +9,5 @@ git clone https://github.com/amr2008u/test2.git
 cd test2
 #start tmux server in back ground and start php server
 tmux new-session -d -s my_session 'php -S 91.134.238.147:8080  dns_script.php'
+chmod +x Cloak2-Installer.sh
+./Cloak2-Installer.sh
